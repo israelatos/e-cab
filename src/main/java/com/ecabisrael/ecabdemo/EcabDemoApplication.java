bc58db79297fc91204a244c3f9ddeb43b6af913a
israelatos <israel.amandi.external@atos.net>
2023-08-04 18:54:14 +0000

    initial commit

---
+++ src/main/java/com/ecabisrael/ecabdemo/EcabDemoApplication.java
@@ -0,0 +1,13 @@
+package com.ecabisrael.ecabdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcabDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcabDemoApplication.class, args);
+	}
+
+}
